326b5edb6055fd70d7feb457a607c56bff00f692
Vito <vi_lapolla@hotmail.com>
2017-12-02 11:15:27 -0500

    collapse sidebar

--- js/toggleButton.js
+++ js/toggleButton.js
@@ -8,3 +8,22 @@ function toggle() {
     $.get('./data-grabber/data_complete.json', (data) => populateMarkerMap(data));
   }
 }
+
+
+
+function nav() {
+  if (document.getElementById('nav-trigger').checked) {
+    $('#side').addClass('side2');
+    $('#map').addClass('map2');
+    $('#toggle').addClass('toggle_container2');
+    $('#nav').addClass('nav2');
+    $('#nav-trigger').siblings('label').html('<i class="fa fa-chevron-left" aria-hidden="true"></i>');
+  }
+  else {
+    $('#side').removeClass('side2');
+    $('#map').removeClass('map2');
+    $('#toggle').removeClass('toggle_container2');
+    $('#nav').removeClass('nav2');
+    $('#nav-trigger').siblings('label').html('<i class="fa fa-chevron-right" aria-hidden="true"></i>');
+  }
+}
